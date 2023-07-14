9cbf75a53ad57265b920ca7145a6a8d2c2f2fabb
chiragnc22 <chiragnc22@gmail.com>
2023-07-14 18:16:31 +0530

    tweaks

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,6 +9,7 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
+    console.log(selectedYear);
   }
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
